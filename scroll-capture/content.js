6fd8239e9b904ad60e6f7847c901873d2fe945f6
Trin Villaruel <trinityvillaruel@gmail.com>
2026-02-15 23:57:24 +0000

    Add non-uniform scroll

--- scroll-capture/content.js
+++ scroll-capture/content.js
@@ -1,22 +1,54 @@
+let running = false;
+let rafId = null;
+
+let speed = 120;          // px/sec (changes over time)
+let lastTs = 0;
+let pausedUntil = 0;
+
 let timer = null;
 
-//Starts scrolling
-function startScroll() { 
-  if (timer) return; 
-  //every 16ms (~60fps) run function and scroll down page by 10 px
-  timer = setInterval(() => {window.scrollBy(0, 10)}, 16);  
-}
+//Update scrolling time
+function frameUpdate(ts){
+    if (!running) return;
 
-//Stops scroll and repeating loop
-function stopScroll() { 
-  if (!timer) return; 
-  clearInterval(timer); 
-  timer = null; 
+    if (!lastTs) lastTs = ts;
+    const dt = (ts - lastTs) / 1000;//s since previous frame update
+    lastTs = ts;
+
+  //Pause randomly
+  if (Date.now() < pausedUntil) {
+    rafId = requestAnimationFrame(frameUpdate);
+    return;
+  }
+  if (Math.random() < 0.02) { //condition is true about 2% of frames
+    pausedUntil = Date.now() + (200 + Math.random() * 800);//pause length: 0.2–1 second
+    rafId = requestAnimationFrame(frameUpdate);
+    return;
+  }
+
+  //Wander speed a bit
+  speed += (Math.random() * 2 - 1) * 50 * dt;
+  speed = Math.max(60, Math.min(400, speed));  //never above 220 never below 60
+
+  window.scrollBy(0, speed * dt);//scroll down only
+  rafId = requestAnimationFrame(frameUpdate);
 }
 
 //Listens for messages from background/popup.js
 chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
-  if (msg.type == "START_SCROLL") startScroll();
-  if (msg.type == "STOP_SCROLL") stopScroll();
-  sendResponse({ ok: true });
+  if (msg.type == "START_SCROLL"){
+    running = true;
+    lastTs = 0;
+    pausedUntil = 0;
+    if (!rafId) rafId = requestAnimationFrame(frameUpdate);
+    sendResponse({ ok: true });
+  }
+
+  if (msg.type == "STOP_SCROLL") {
+    running = false;
+    if (rafId) cancelAnimationFrame(rafId);
+    rafId = null;
+    sendResponse({ ok: true });
+    return;
+  }
 });
